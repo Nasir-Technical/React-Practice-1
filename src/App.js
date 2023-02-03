16f068e708f5987a63c848630d366ec86786869b
Nasir-Technical <nasir.18.2006@gmail.com>
2023-02-03 15:54:52 +0500

    react practice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,141 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+let mobiles = [
+  {
+    name: "a30",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 2000,
+    brand: "samsung",
+  },
+  {
+    name: "note10",
+    ram: "6gb",
+    rom: "128gb",
+    camera: "50px",
+    price: 4000,
+    brand: "samsung",
+  },
+  {
+    name: "s10",
+    ram: "3gb",
+    rom: "128gb",
+    camera: "10px",
+    price: 5000,
+    brand: "samsung",
+  },
+  {
+    name: "iphone4",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 10000,
+    brand: "iphone",
+  },
+  {
+    name: "iphone4s",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 20000,
+    brand: "iphone",
+  },
+  {
+    name: "iphone5",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 30000,
+    brand: "iphone",
+  },
+  {
+    name: "iphone6",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 30000,
+    brand: "iphone",
+  },
+  {
+    name: "redminote10",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 10000,
+    brand: "xiaomi",
+  },
+  {
+    name: "redminote11",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 10000,
+    brand: "xiaomi",
+  },
+  {
+    name: "redmi10",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 10000,
+    brand: "xiaomi",
+  },
+  {
+    name: "redmi10pro",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 10000,
+    brand: "xiaomi",
+  },
+  {
+    name: "a3s",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 10000,
+    brand: "oppo",
+  },
+  {
+    name: "redmi10pro",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 10000,
+    brand: "xiaomi",
+  },
+  {
+    name: "a3s",
+    ram: "4gb",
+    rom: "64gb",
+    camera: "50px",
+    price: 10000,
+    brand: "oppo",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <section className="box" >
+          {mobiles.map((x) => {
+            return (
+              <div className="mobile">
+                <h1>{x.brand}</h1>
+                <p>{x.name}</p>
+                <p>Rs {x.price}</p>
+              </div>
+
+            )
+          })}
+        </section>
+
       </header>
     </div>
   );
